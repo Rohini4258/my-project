ea6142f90bd442fda3b1d73cd618d952d2c1e784
Rohini4258 <rohini4258@gmail.com>
2024-10-04 20:27:26 -0400

    new changes in build file

--- src/ProjectTwo/obj/Debug/net8.0/ProjectTwo.AssemblyInfo.cs
+++ src/ProjectTwo/obj/Debug/net8.0/ProjectTwo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectTwo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b1d5cba0925ab1930cc413cc4f2eb39140ad582")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ea47a5a20cc8bddb9a2ff27730ec97bb5899a78")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectTwo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectTwo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
